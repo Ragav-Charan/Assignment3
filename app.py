a5e10a2c383c0221eaf97dcec30cd3f0d768d8d3
Charan-Venkatesan <charan.venkatesan94@gmail.com>
2019-06-17 18:41:18 -0500

    Version 3

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import pandas as pd
 import redis as redis
 from flask import Flask, render_template, request
 import sqlite3 as sql
+
+from math import radians, sin, cos, sqrt, atan2
+
 app = Flask(__name__)
 port = int(os.getenv('VCAP_APP_PORT','5000'))
 myHostname = "flyingjaguar.redis.cache.windows.net"
@@ -41,6 +44,19 @@ def records():
 @app.route('/restricted')
 def restricted():
     return render_template('rest.html')
+
+@app.route('/restrictedmag')
+def restrictedmag():
+    return render_template('restmag.html')
+
+@app.route('/dist')
+def dist():
+    return render_template('location.html')
+
+
+@app.route('/time')
+def time1():
+    return render_template('time.html')
 
 
 @app.route('/options', methods=['POST', 'GET'])
@@ -96,6 +112,100 @@ def options2():
     elapsed_time = end_time - start_time
     return render_template("list2.html", rows= [rows,elapsed_time])
 
+
+@app.route('/options3', methods=['POST', 'GET'])
+def options3():
+    start_time = time.time()
+    mag1 = request.form['mag1']
+    mag2 = (request.form['mag2'])
+    rows = []
+    num = int(request.form['num'])
+    for i in range(num):
+        cur = cnxn.cursor()
+        c = "select * from all_month WHERE mag between "+mag1+" and "+mag2
+       # cur.execute("select * from all_month WHERE place LIKE ?", ('%'+loc+'%',))
+        if r.get(c):
+            print('Cached')
+            rows.append(r.get(c))
+        else:
+            print('Not Cached')
+            cur.execute("select * from all_month WHERE mag between ? and ?", (mag1, mag2))
+            get = cur.fetchall();
+            rows.append(get)
+            r.set(c,str(get))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return render_template("list3.html", rows=[rows,elapsed_time])
+
+
+@app.route('/distance',methods = ['POST', 'GET'])
+def distance():
+   start_time = time.time()
+   print (float(request.form['lat1']))
+   print (float(request.form['lon1']))
+   print (float(request.form['kms']))
+   lat1 = float(request.form['lat1'])
+   lon1 = float(request.form['lon1'])
+   cur = cnxn.cursor()
+   b = "select * from all_month where latitude  = "+request.form['lat1']+" and longitude = "+request.form['lon1']+"'"
+   #cur.execute("select * from all_months ")
+   #rows = cur.fetchall()
+   #ref:https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+   R = 6373.0
+
+   dist =[]
+   rows = []
+   if r.get(b):
+       if(r.get(b) <= (float(request.form['kms']))):
+           print("cache")
+
+   else:
+       cur.execute("select * from all_month where latitude  = ? and longitude = ?",(request.form['lat1'], request.form['lon1'],))
+       data = cur.fetchall()
+       lat_r = radians(float(request.form['lat1']))
+       lon_r = radians(float(request.form['lon1']))
+       for row in data:
+           lat2 = radians(row[2])
+           lon2 = radians(row[3])
+           dlon = lon2 - lon1
+           dlat = lat2 - lat1
+           a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+           c = 2 * atan2(sqrt(a), sqrt(1 - a))
+           distance = float(R * c)
+           if (distance <= (float(request.form['kms']))):
+               dist.append(row)
+               r.set(b, distance)
+           #rows.append(get)
+   end_time = time.time()
+   elapsed_time = end_time - start_time
+   return render_template("list4.html", elapsed_time=elapsed_time, rows1=dist)
+
+
+
+@app.route('/otr',methods=['POST', 'GET'])
+def otr():
+    start_time = time.time()
+    num = int(request.form['num'])
+    time1 = request.form['time1']
+    time2 = request.form['time2']
+    rows = []
+    for i in range(num):
+        cur = cnxn.cursor()
+        b = "select * from all_month WHERE time BETWEEN "+ time1 +" and " + time2
+        # cur.execute("select * from all_month WHERE time BETWEEN ? and ?", (time1,time2))
+        print(rows)
+        if r.get(b):
+            print('Cached')
+            rows.append(r.get(b))
+        else:
+            print('Not Cached')
+            cur.execute("select * from all_month WHERE time BETWEEN ? and ?", (time1,time2))
+            get = cur.fetchall();
+            rows.append(get)
+            r.set(b, str(get))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return render_template("list5.html", dist=rows, etime=elapsed_time)
 
 if __name__ == '__main__':
     #app.run(default=True)
